8c41e2068cf68452a618dbc2675350b6b77d35d3
Leonel Alegre <78889336+LeonelAlegreDev@users.noreply.github.com>
2024-09-05 16:35:48 +0000

    Se realizo el ejercicio de la clase ej-01

--- Ejercicios/ej-01/src/app/app.component.ts
+++ Ejercicios/ej-01/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ej-01';
+  edadUno: number = 0;
+  edadDos: number = 0;
+
+  suma: number = 0;
+  promedio: number = 0;
+
+  Calcular(): void {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+  Limpiar(): void {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.suma = 0;
+    this.promedio = 0;
+  }
 }
---
+++ Ejercicios/ej-01/src/app/usuario/usuario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario.component.html',
+  styleUrl: './usuario.component.css'
+})
+export class UsuarioComponent {
+  nombre: string = '';
+  clave: string = '';
+}
